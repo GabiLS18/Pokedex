ba092113dbcf503de8c34975910b26366cff7176
Gabrielle <gabi_luz25@hotmail.com>
2022-12-19 14:43:18 -0300

    Pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
-console.log ('sucess');
+
+function convertPokemonToLi(pokemon) {
+    return `
+      <ol class="pokemons">
+      <li class="pokemon">
+          <span class="number">#001</span>
+          <span class="name">${pokemon.name}</span>
+          <div class="detail">
+              <ol class="types">
+                  <li class="type">grass</li>
+                  <li class="type">poison</li>
+              </ol>
+              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png"
+                  alt="${pokemon.name}">
+          </div>
+          </li>
+          `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+
+})
+
